6a68b810c215e4f746cb95ebf3272c5f26ef9b36
Juan Pablo Civile <elementohb@gmail.com>
2011-03-13 17:00:47 -0300

    First version with sockets

--- server/tank.py
+++ server/tank.py
@@ -1,12 +1,20 @@
-from subprocess import Popen, PIPE
+from subprocess import Popen
+from socket import socket
 
 class Tank(object):
 
     def __init__(self, team, command, range):
         self.__team = team
-        self.__process = Popen(command.split('\n')[0].split(' '), stdin=PIPE, stdout=PIPE, stderr=PIPE)
+
+        self.__socket = socket()
+        self.__socket.bind(('localhost', '5999'))
+        self.__socket.listen(1)
+
+        self.__process = Popen(command.split('\n')[0].split(' '))
         self.__alive = True
 
+        self.__socket.accept()
+        self.__file = self.__socket.makefile()
         self.send_response([team, range])
 
     def get_team(self):
@@ -15,6 +23,7 @@ class Tank(object):
     def kill(self):
         """ OH NOES """
         self.__process.kill()
+        self.__socket.close()
         self.__alive = False
 
     def is_alive(self):
@@ -24,7 +33,7 @@ class Tank(object):
         """ Get a tuple with (action, direction) with the tanks next action """
         if not self.__value:
             return None
-        values = self.__process.stdin.readline().split(' ')[:2]
+        values = self.__file.readline().split(' ')[:2]
         return (values[0], values[1])
 
     def send_response(self, response):
@@ -36,5 +45,4 @@ class Tank(object):
         if isinstance(response, basestring):
             response = [response]
 
-        self.__process.stdout.write(' '.join([str(value) for value in response]) + '\n')
-        self.__process.stdout.flush()
+        self.__file.write(' '.join([str(value) for value in response]) + '\n')
