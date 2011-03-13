6f229faf63ee98c462b71af10d63a52adc53b180
Esteban Ordano <eordano@gmail.com>
2011-03-13 18:07:20 -0300

    Modified example, deleted cpp version, changed things in C++

--- clients/example/example.py
+++ clients/example/example.py
@@ -1,21 +1,22 @@
+from sys import argv
 from socket import socket
 
 if __name__ == '__main__':
     socket = socket()
-    socket.connect('localhost', '5999')
+    socket.connect(('localhost', int(argv[1])))
     
-    file = socket.makefile()
-
-    TEAM_ID, SHOOT_RANGE = map(int, file.readline().split(' '))
+    data = socket.recv(1024)
+    TEAM_ID, SHOOT_RANGE = map(int, data.split(' ')[:2])
 
     while True:
         direction = get_random_direction()
 
-        file.write('LASER ' + get_random_direction() + '\n')
-        TANK_ID, TANK_DIS = map(int, file.readline().split(' '))
+        socket.send('LASER ' + get_random_direction() + '\n')
+        data = socket.recv(1024)
+        TANK_ID, TANK_DIS = map(int, data.split(' ')[:2])
 
         if TANK_ID and TANK_ID != TEAM_ID and TANK_DIS < SHOOT_RANGE:
-            file.write('SHOOT ' + direction + '\n')
+            socket.send('SHOOT ' + direction + '\n')
 
-        file.write('MOVE ' + get_random_direction() + '\n')
+        socket.send('MOVE ' + get_random_direction() + '\n')
 
--- server/tank.py
+++ server/tank.py
@@ -2,19 +2,20 @@ from subprocess import Popen
 from socket import socket
 
 class Tank(object):
+    socket_port = 6999
 
     def __init__(self, team, command, range):
         self.__team = team
 
         self.__socket = socket()
-        self.__socket.bind(('localhost', 5999))
+        Tank.socket_port += 1
+        self.__socket.bind(('localhost', Tank.socket_port))
         self.__socket.listen(1)
 
-        self.__process = Popen(command.split('\n')[0].split(' '))
+        self.__process = Popen(command.split('\n')[0].split(' ') + [str(Tank.socket_port)])
         self.__alive = True
 
         self.__socket.accept()
-        self.__file = self.__socket.makefile()
         self.send_response([team, range])
 
     def get_team(self):
@@ -31,9 +32,13 @@ class Tank(object):
 
     def next_action(self):
         """ Get a tuple with (action, direction) with the tanks next action """
-        if not self.__value:
+        if not self.__alive:
             return None
-        values = self.__file.readline().split(' ')[:2]
+        values = None
+        while not values:
+            values = self.__socket.recv(1024)
+        
+        values = valus.split(' ')[:2]
         return (values[0], values[1])
 
     def send_response(self, response):
@@ -45,4 +50,4 @@ class Tank(object):
         if isinstance(response, basestring):
             response = [response]
 
-        self.__file.write(' '.join([str(value) for value in response]) + '\n')
+        self.__socket.send(' '.join([str(value) for value in response]) + '\n')
